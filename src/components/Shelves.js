372a764bbfa3830325021514d72fe454ab1c1982
Ciprian Popa <ciprian_popa2002@yahoo.com>
2022-04-07 16:50:37 -0400

    getting correct shelves

--- src/components/Shelves.js
+++ src/components/Shelves.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 function Shelves(props) {
     let { books, updateController } = props
-    
     const shelves = [
       { title: 'Read', key: 'read' },
       { title: 'Want To Read', key: 'wantToRead' },
@@ -11,46 +10,50 @@ function Shelves(props) {
     
   return (
     <div>
-      <div className="bookshelf">{ shelves.map(shelf => 
+      <div className="bookshelf">
+        {shelves.map(shelf => 
           (<h2 className="bookshelf-title">{ shelf.title }</h2> )
-          )}
+        )}
         <div className="bookshelf-books">
-          
-        </div>
-      </div>
-      <ol className="books-grid">
-        {books.filter(b =>  b.shelf === shelves.key
-        // console.log('b.shelf:', b.shelf, 'b.shelf === shelves.key:', b.shelf === shelves.key, 'books:', books)
-        )
-       
-              .map((book) => (
-            <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div 
-                    className="book-cover"
-                    style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}
-                  ></div>
-                  <div className="book-shelf-changer">
-                    <select         
-                      value={book.shelf || 'none'}    
-                      onChange={(event) => updateController(book, event.target.value)} 
-                    >
-                      <option value="move" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
+          <ol className="books-grid">
+            {shelves.map(shelf => {
+              return (
+                <div>
+                  {books.filter(book => (book.shelf === shelf.key))
+                        .map((book) => ( 
+                              <li key={book.id}>
+                                <div className="book">
+                                  <div className="book-top">
+                                    <div 
+                                      className="book-cover"
+                                      style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}
+                                    ></div>
+                                    <div className="book-shelf-changer">
+                                      <select         
+                                        value={book.shelf || 'none'}    
+                                        onChange={(event) => updateController(book, event.target.value)} 
+                                      >
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                      </select>
+                                    </div>
+                                  </div>
+                                  <div className="book-title">{book.title}</div>
+                                  <div className="book-authors">{book.authors}</div>
+                                </div>
+                              </li> 
+                            ))
+                          }
+                        </div>
+                      )
+                    })}
+                  </ol>
                 </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors}</div>
               </div>
-            </li>
-          ))}
-      </ol>
-    </div>
-  )
-}
+            </div>
+          )
+        }
 export default Shelves;
