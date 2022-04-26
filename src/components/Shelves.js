d7dae4b50e0d2ce742af6165720cd0c38b45accc
Ciprian Popa <ciprian_popa2002@yahoo.com>
2022-04-26 17:12:17 -0400

    erasing a map function

--- src/components/Shelves.js
+++ src/components/Shelves.js
@@ -12,48 +12,44 @@ function Shelves(props) {
     <div>
       <div className="bookshelf">
         {shelves.map(shelf => 
-          (<h2 className="bookshelf-title">{ shelf.title }</h2> )
-        )}
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {shelves.map(shelf => {
-              return (
+          (<h2 className="bookshelf-title">{ shelf.title }</h2>
+            <div className="bookshelf-books"> 
+              <ol className="books-grid">
                 <div>
-                  {books.filter(book => (book.shelf === shelf.key))
-                        .map((book) => ( 
-                              <li key={book.id}>
-                                <div className="book">
-                                  <div className="book-top">
-                                    <div 
-                                      className="book-cover"
-                                      style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}
-                                    ></div>
-                                    <div className="book-shelf-changer">
-                                      <select         
-                                        value={book.shelf || 'none'}    
-                                        onChange={(event) => updateController(book, event.target.value)} 
-                                      >
-                                        <option value="move" disabled>Move to...</option>
-                                        <option value="currentlyReading">Currently Reading</option>
-                                        <option value="wantToRead">Want to Read</option>
-                                        <option value="read">Read</option>
-                                        <option value="none">None</option>
-                                      </select>
-                                    </div>
-                                  </div>
-                                  <div className="book-title">{book.title}</div>
-                                  <div className="book-authors">{book.authors}</div>
-                                </div>
-                              </li> 
-                            ))
-                          }
+                  {books
+                    .filter(book => (book.shelf === shelf.key))
+                    .map((book) => ( 
+                      <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div 
+                              className="book-cover"
+                              style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}
+                            ></div>
+                            <div className="book-shelf-changer">
+                              <select         
+                                value={book.shelf || 'none'}    
+                                onChange={(event) => updateController(book, event.target.value)} 
+                              >
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
                         </div>
-                      )
-                    })}
-                  </ol>
-                </div>
+                      </li> 
+                    ))}
+                  </div>
+                </ol>
               </div>
+              ))}
             </div>
-          )
-        }
+          </div>
+  )
+}
 export default Shelves;
