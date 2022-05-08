a992506129238786ac348d6a383921fb541377a2
Ciprian Popa <ciprian_popa2002@yahoo.com>
2022-05-08 12:28:31 -0400

    Finished requirements

--- src/components/Shelves.js
+++ src/components/Shelves.js
@@ -9,44 +9,42 @@ function Shelves(props) {
     ]
     
     return (
-      <>
+      <div className="bookshelf">
         {shelves.map((shelf) => (
-          <div className="bookshelf">
+          <>
             <h2 className="bookshelf-title">{shelf.title}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                <div>
-                  {books
-                    .filter((book) => book.shelf === shelf.key)
-                    .map((book) => (
-                      <li key={book.id}>
-                        {console.log("book:", book)}
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
-                            <div className="book-shelf-changer">
-                              <select value={book.shelf || "none"} onChange={(event) => updateController(book, event.target.value)}>
-                                <option value="move" disabled>
-                                  Move to...
-                                </option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
+                {books
+                  .filter((book) => book.shelf === shelf.key)
+                  .map((book) => (
+                    <li key={book.id}>
+                      {console.log("book:", book)}
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
+                          <div className="book-shelf-changer">
+                            <select value={book.shelf || "none"} onChange={(event) => updateController(book, event.target.value)}>
+                              <option value="move" disabled>
+                                Move to...
+                              </option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
                           </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors}</div>
                         </div>
-                      </li>
-                    ))}
-                </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>
+                  ))}
               </ol>
             </div>
-          </div>
+          </>
         ))}
-      </>
+      </div>
     )
 }
 export default Shelves;
