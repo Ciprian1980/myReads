299684d50d92301a83618ac8fe25ee96d1af1057
Ciprian Popa <ciprian_popa2002@yahoo.com>
2022-05-11 21:01:51 -0400

    adding hover on book

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-function DetailPage() {
+function DetailPage(props) {
+  const { books } = props
   return (
     <div className="main-description-page">
       <div className="description-book">
         <ul className="description-list">
           <li>
             <h1 className="list-books-title">My Reads</h1>
-            
+            <h5>Book Details</h5>
+            <h4>Title: {books.title}</h4>
+            <h4>Subtitle: {books.subtitle}</h4>
+            <h4>Authors: {books.authors}</h4>
+            <h4>Publisher: {books.publisher}</h4>
+            <h4>Description: {books.description}</h4>
           </li>
         </ul>
       </div>
