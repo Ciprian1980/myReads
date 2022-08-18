3de643164e18b3ad9f299798e0724ef6de70bc11
Ciprian Popa <ciprian_popa2002@yahoo.com>
2022-08-18 12:48:55 -0400

    Initial commit

---
+++ build/precache-manifest.e8eac3570f88475b67b3a1a55ffaf352.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba136ed423daaee61ff3",
+    "url": "/static/css/main.cf0eb9e9.chunk.css"
+  },
+  {
+    "revision": "ba136ed423daaee61ff3",
+    "url": "/static/js/main.ba136ed4.chunk.js"
+  },
+  {
+    "revision": "0b410a93596ea3414b2e",
+    "url": "/static/js/1.0b410a93.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "c8706123db0d925d6fa88965cd8b4e6f",
+    "url": "/index.html"
+  }
+];
